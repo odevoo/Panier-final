1b422aba3f466d07ecfdc1085b024244497027c9
odevoo <elandry@odevoo.com>
2016-10-08 09:07:43 +0200

    cart bug fixed

--- js/script.js
+++ js/script.js
@@ -18,40 +18,17 @@ var drink = { label: "Boisson", type: "Boisson", prix: 5, img: "img/drink.jpg", 
 var fish = {label: "poisson", type: "plat", prix: 5, img: "img/fish.jpg", qt: 0};
 var cheese = {label: "fromage", type: "dessert", prix: 5, img: "img/cheese.jpg", qt: 0};
 
-
-$('#img-salad').on('click', clickAcheter);
-$('#img-steak').on('click', clickAcheter);
-$('#img-icecream').on('click', clickAcheter);
-$('#img-drink').on('click', clickAcheter);
-$('#img-fish').on('click', clickAcheter);
-$('#img-cheese').on('click', clickAcheter);
+$('#img-Salade').on('click', function() { ajouterArticle(salad); });
+$('#img-Steak').on('click', function() { ajouterArticle(steak); });
+$('#img-Glace').on('click', function() {  ajouterArticle(icecream); });
+$('#img-Boisson').on('click', function() { ajouterArticle(drink); });
+$('#img-poisson').on('click', function() { ajouterArticle(fish); });
+$('#img-fromage').on('click', function() { ajouterArticle(cheese); });
 
 $('#cart-top').on('click', function() {
   $('#cart-detail').toggle();
 });
 
-
-
-function clickAcheter() {
-  if(this.id == 'img-salad') {
-    ajouterArticle(salad);
-  }
-  if(this.id == 'img-steak') {
-    ajouterArticle(steak);
-  }
-  if(this.id == 'img-icecream') {
-    ajouterArticle(icecream);
-  }
-  if(this.id == 'img-drink') {
-    ajouterArticle(drink);
-  }
-  if(this.id == 'img-fish') {
-    ajouterArticle(fish);
-  }
-  if(this.id == 'img-cheese') {
-    ajouterArticle(cheese);
-  }
-}
 
 function ajouterArticle(article) {
   panier.push(article);
@@ -63,12 +40,9 @@ function updatepanier(article) {
   for(var i = 0; i < panier.length; i++) {
     totalPrix += panier[i].prix;
     }
-
   $('#totarticle').text("  " + panier.length + "  ");
   $('#totprice').text("   " + totalPrix + "   ");
-  
   article.qt += 1;
-
   ajouterLigneDetail(article);
 
 }
@@ -78,47 +52,32 @@ function ajouterLigneDetail(article) {
   tr = $("<tr></tr>");
 
   if (article.qt == 1) {
-
   var tdImage = $("<td></td>");
-
   var img = $("<img>");
   img.attr("src", article.img);
-
   tdImage.append(img);
-
   tr.append(tdImage);
-  
-
   var tdLabel = $("<td></td>");
   var label = $("<span></span>");
   label.text(article.label);
   tdLabel.append(label);
   tr.append(tdLabel);
-  
-
   var tdType = $("<td></td>");
   var type = $("<span></span>");
   type.text(article.type);
   tdType.append(type);
   tr.append(tdType);
-
-  
-
   var tdqantity = $("<td></td>");
   quantity = $("<span></span>");
   quantity.text(article.qt);
   quantity.attr("id", "QT-" + article.label);
   tdqantity.append(quantity);
   tr.append(tdqantity);
-  
-  
-
   var tdPrix = $("<td></td>");
   var prix = $("<span></span>");
   prix.text(article.prix.toString());
   tdPrix.append(prix);
   tr.append(tdPrix);
-
   var tdPrixTotal = $("<td></td>");
   prixTotal = $("<span></span>");
   prixTotal.text(article.prix*article.qt);
